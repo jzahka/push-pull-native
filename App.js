8b55b95ad696bfbf6a9e5b6c12f26885ce7cc606
John Zahka <john.zahka@gmail.com>
2018-08-07 20:55:29 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Alert, Button, View } from 'react-native';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Button
+          onPress={() => {
+            Alert.alert('You tapped the button!');
+          }}
+          title="Press Me"
+        />
+      </View>
+    );
+  }
+}
